62f21c9e676cc643b13950f92df102f4d07ff5f7
Krati67 <51830489+Krati67@users.noreply.github.com>
2020-11-07 14:12:56 +0530

    Update participants.js

--- js/participants.js
+++ js/participants.js
@@ -1,9 +1,17 @@
 const participants = [
-
+{
+    name: "Punit Baviskar",
+    college: "Amity University Mumbai",
+    city: "Pune",
+    github: "puneetb22",
+    linkedin: "https://www.linkedin.com/in/puneetb22/",
+    website: "https://punitbaviskar.me",
+  }, 
+  
   {
     name: "Krati Rastogi",
     college: "Amity University Mumbai",
-    city: "PAhmedabad",
+    city: "Ahmedabad",
     github: "Krati67",
     linkedin: "https://www.linkedin.com/in/krati-rastogi-836983181/",
   
