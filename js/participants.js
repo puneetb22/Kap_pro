e987fdbaf108065c1c2e532815c5478a7474b97b
Atharv <Atharvkaveeshwar257@gmail.com>
2020-11-07 13:48:13 +0530

    add me as a developer

--- js/participants.js
+++ js/participants.js
@@ -8,4 +8,12 @@ const participants = [
     linkedin: "https://www.linkedin.com/in/puneetb22/",
     website: "https://punitbaviskar.netlify.app/",
   },
+  {
+    name: "Atharv Kaveeshwar",
+    college: "Amity University Mumbai",
+    city: "Indore",
+    github: "kaveeshwarji",
+    linkedin: "https://www.linkedin.com/in/atharv-kaveeshwar/",
+    website: "",
+  }
 ];
